da68771ef9cfb1823ad805a1a072cb21e0b08869
techicsdev <ja@sk-associates.org>
2020-02-12 18:41:05 +0600

    change config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,28 +1,13 @@
 // .vuepress/config.js
 module.exports = {
+	base:"/awesome-node/",
 	themeConfig: {
 		sidebar: [
 			'/',
 			['/resources/webframeworks', 'web-frameworks'],
 		],
-		// Assumes GitHub. Can also be a full GitLab url.
-		repo: 'team-mahal/awesome-node',
-		// Customising the header label
-		// Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
 		repoLabel: 'Contribute!',
-
-		// Optional options for generating "Edit this page" link
-		base:"/awesome-node/",
-
-		// if your docs are in a different repo from your main project:
-		docsRepo: 'team-mahal/awesome-node',
-		// if your docs are not at the root of the repo:
-		docsDir: 'awesome-node/docs',
-		// if your docs are in a specific branch (defaults to 'master'):
-		docsBranch: 'master',
-		// defaults to false, set to true to enable
 		editLinks: true,
-		// custom text for edit link. Defaults to "Edit this page"
 		editLinkText: 'Help us improve this page!',
 	}
 }
