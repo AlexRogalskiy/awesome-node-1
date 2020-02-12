189fdab061272e1177f683bb3a1d3a99dd9371b6
techicsdev <ja@sk-associates.org>
2020-02-12 15:21:26 +0600

    change repo config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -16,12 +16,13 @@ module.exports = {
 		// if your docs are in a different repo from your main project:
 		docsRepo: 'team-mahal/awesome-node',
 		// if your docs are not at the root of the repo:
-		docsDir: '.vuepress/dist',
+		docsDir: 'awesome-node/docs',
 		// if your docs are in a specific branch (defaults to 'master'):
 		docsBranch: 'master',
 		// defaults to false, set to true to enable
 		editLinks: true,
 		// custom text for edit link. Defaults to "Edit this page"
-		editLinkText: 'Help us improve this page!'
+		editLinkText: 'Help us improve this page!',
+		base:'/awesome-node/'
 	}
 }
